631b048a5edb7d79a4bb67adb838e2664a765a67
npiscitello500 <87912445+npiscitello500@users.noreply.github.com>
2023-04-25 01:29:05 -0300

    TrabajoIntegradorEntrega2

---
+++ TrabajoIntegradorEntrega2/src/main/java/com/mycompany/trabajointegradorentrega2/TrabajoIntegradorEntrega2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajointegradorentrega2;
+
+/**
+ *
+ * @author Admin
+ */
+public class TrabajoIntegradorEntrega2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
